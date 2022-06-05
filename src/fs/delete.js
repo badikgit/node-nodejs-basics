00cce730b9288aabed1807bbd550ae08f56d457a
badikgit <stas_yar@tut.by>
2022-06-05 10:41:22 +0300

    feat: task fs/delete is done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,32 @@
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+import { stat, rm } from 'fs/promises';
+
 export const remove = async () => {
-    // Write your code here 
+  console.log(`\n4. Run: ${fileURLToPath(import.meta.url)}`);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const fileName = resolve(__dirname, "files", "fileToRemove.txt");
+
+  try {
+    console.info(`\x1b[33mDeleting file: \x1b[0m${fileName}`);
+    if ((await stat(fileName)).isFile()) {
+      await rm(fileName);
+      console.log("\x1b[32m\x1b[7m Completed \x1b[0m");
+    }
+    else {
+      throw (new Error("\x1b[41m\x1b[37m FS operation failed. \x1b[0m"));
+    }
+  }
+  catch (error) {
+    if (error.code === "ENOENT") {
+      throw (new Error("\x1b[41m\x1b[37m FS operation failed. \x1b[0m"));
+      //throw new Error("\x1b[41m\x1b[37m FS operation failed \x1b[0m");
+    }
+    else {
+      throw (error);
+      //throw error;
+    }
+  }
 };
