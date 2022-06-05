181e979403b01a7a477419094ab85a538197b4a3
badikgit <stas_yar@tut.by>
2022-06-06 02:25:50 +0300

    feat: task cli/args is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+import { fileURLToPath } from 'url'
+import { argv } from "process";
+
 export const parseArgs = () => {
-    // Write your code here 
+  console.log(`\n2. Run: ${fileURLToPath(import.meta.url)}`);
+  console.log(`\x1b[33mParsing argv:\x1b[0m ${argv}`);
+  console.log('Result:' + ' ' + argv
+      .filter((item, index) => index > 1)
+      .map((item, index) => index%2 ? `is ${item},` : item.slice(2))
+      .join(' ')
+      .slice(0,-1));
+  console.log("\x1b[32m\x1b[7m Completed \x1b[0m");
 };
+
+parseArgs();
