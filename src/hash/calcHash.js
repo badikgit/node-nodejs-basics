5a3209dd3e758972b621d2c60e1ce42ce3ba0579
badikgit <stas_yar@tut.by>
2022-06-05 18:05:34 +0300

    feat: task hash/calculateHash is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  console.log(`\n1. Run: ${fileURLToPath(import.meta.url)}`);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const pathToFile = resolve(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  try {
+    console.log(`\x1b[33mCalculating hash of file \x1b[0m${pathToFile}`);
+    const data = await readFile(pathToFile, { encoding: 'utf8' });
+    const hash = createHash('sha256').update(data).digest('hex');
+    console.log(`Hash: ${hash}\n\x1b[32m\x1b[7m Completed \x1b[0m`);
+    return hash;
+  } catch (error) {
+    throw error;
+  }
 };
