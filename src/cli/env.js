21d1a5e75d9392e1b09659737fba66c9eb3ae3a1
badikgit <stas_yar@tut.by>
2022-06-06 15:09:01 +0300

    feat: task cli/env is done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+//import { fileURLToPath } from 'url'
+import { env } from "process";
+
 export const parseEnv = () => {
-    // Write your code here 
+  //console.log(`\n1. Run: ${fileURLToPath(import.meta.url)}`);
+  //console.log("\x1b[33mParsing env.\x1b[0m");
+  console.log(/* 'src/cli/env.js Result:' + ' ' +  */Object.entries(env)
+      .filter(([key, value]) => key.slice(0, 4) === "RSS_")
+      .map(([key, value], index) => [`${key}=${value}`])
+      .join('; ')
+  );
+  //console.log("\x1b[32m\x1b[7m Completed \x1b[0m");
 };
+parseEnv();
