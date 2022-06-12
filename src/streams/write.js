4408e844bf959ab5c85fa5b0175e9355e5764b80
badikgit <stas_yar@tut.by>
2022-06-12 16:27:51 +0300

    feat: task straems/write is done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,29 @@
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+import { WriteStream } from 'fs';
+import readline from 'readline';
+import { stdin as input, stdout as output } from 'process';
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filePath = resolve(__dirname, "files", "fileToWrite.txt");
+  const writeStream = WriteStream(filePath);
+  const readlineStream = readline.createInterface({ input, output });
+
+  console.log(`\x1b[33mWriting to file: \x1b[0m${filePath}\n`);
+  readlineStream.on('line', input => writeStream.write(`${input}\n`));
+
+  readlineStream.on('SIGINT', () => readlineStream.close());
+
+  readlineStream.on('close', () => {
+    writeStream.close();
+    console.log("\n\x1b[32m\x1b[7m Completed \x1b[0m");
+  });
+
+  readlineStream.on('error', (error) => { throw error });
+  writeStream.on('error', (error) => { throw error });
 };
+
+write();
