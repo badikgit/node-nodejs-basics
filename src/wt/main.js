74a808ee8efb3cc7153093637d6d7d5c3fa6097a
badikgit <stas_yar@tut.by>
2022-06-12 18:07:04 +0300

    feat: task wt/main is done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,31 @@
+import { Worker } from 'worker_threads';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { rejects } from 'assert';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const workerPath = resolve(__dirname, 'worker.js');
+
+  const cp = cpus();
+  let number = 10;
+
+  const workersResults = await Promise.allSettled( cp.map( () => {
+    return new Promise((res, rej) => {
+      const worker = new Worker(workerPath, { workerData: number++ });
+      worker.on('error', error => rej(error));
+      worker.on('message', message => res(message));
+    });
+  }))
+  const results = workersResults.map(({status, value}) => ({
+    status: status === "fulfilled" ? "resolved" : "error",
+    data: status === "fulfilled" ? value : null
+  }));
+
+  console.log(results);
+  return results;
 };
+
+performCalculations();
