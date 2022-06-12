4bb42161dc5016e45542a24dba3d144e23707a4a
badikgit <stas_yar@tut.by>
2022-06-12 19:10:44 +0300

    feat: task cp/spawnChildProcess is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const scriptPath = resolve(__dirname, 'files', 'script.js');
+
+  const childProcess = fork(scriptPath, args.split(' '));
+
+  childProcess.on('data', data => {
+    process.stdout.write(data);
+  });
+  childProcess.send(process.stdin);
 };
+
+spawnChildProcess('--paramName1 name1 --paramName2 name2');
